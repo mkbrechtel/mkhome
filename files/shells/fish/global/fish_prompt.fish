4e93be995ded763c18716dedc455b4d902041ed2
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2022-01-15 23:17:41 +0100

    shell setup

--- files/shells/fish/global/fish_prompt.fish
+++ files/shells/fish/global/fish_prompt.fish
@@ -5,23 +5,23 @@ function fish_prompt --description 'prompt'
 		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
 	end
 
-	set -l color_cwd
+	set -l color_prompt
 	set -l suffix
 	switch $USER
 	case root toor
 		if set -q fish_color_cwd_root
-			set color_cwd $fish_color_cwd_root
+			set color_prompt $fish_color_cwd_root
 		else
-			set color_cwd $fish_color_cwd
+			set color_prompt $fish_color_cwd
 		end
 		set suffix '#'
 	case '*'
 		echo -n -s "$USER" @
-		set color_cwd $fish_color_cwd
+		set color_prompt $fish_color_cwd
 		set suffix '>'
 	end
 
   set -g fish_prompt_pwd_dir_length 0
 
-	echo -n -s "$__fish_prompt_hostname" : (set_color $color_cwd) (prompt_pwd) (set_color normal) " $suffix "
+	echo -n -s "$__fish_prompt_hostname" : (prompt_pwd) (set_color normal) " " (set_color $color_prompt) "$suffix "
 end
